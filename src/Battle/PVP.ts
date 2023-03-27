6e3db5d18085ee4da8d61a8ded8feac5fdb69d89
Pedro Silva <pvasilva.dev@hotmail.com>
2023-03-27 16:25:25 -0300

    feat: add PVP class

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,22 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  private _playerOne: Fighter;
+  private _playerTwo: Fighter;
+
+  constructor(playerOne: Fighter, playerTwo: Fighter) {
+    super(playerOne);
+    this._playerOne = playerOne;
+    this._playerTwo = playerTwo;
+  }
+
+  override fight(): number {
+    while (this._playerOne.lifePoints > 0 && this._playerTwo.lifePoints > 0) {
+      this._playerOne.attack(this._playerTwo);
+      this._playerTwo.attack(this._playerOne);
+    }
+    
+    return super.fight();
+  }
+}
